fb2fff2eae7108d1426295c0c853c89295e843ba
aroop <aroop@netopss-MacBook-Pro-5.local>
2020-02-04 15:18:45 +0530

    .

--- policies/jaeger-policy.js
+++ policies/jaeger-policy.js
@@ -1,4 +1,6 @@
 var jaeger = require('./jaeger')
+const jsonParser = require('express').json();
+const urlEncoded = require('express').urlencoded({ extended: true });
 const tracer = jaeger("anuvaad");
 const { PassThrough } = require('stream');
 
@@ -6,33 +8,41 @@ module.exports = {
     name: 'jaeger-policy',
     policy: (actionParams) => {
         return (req, res, next) => {
-            var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-            var method = req.method;
-            var url = req.url;
-            var referer = req.headers.referer || "";
-            var ua = req.headers['user-agent'];
-            let span = tracer.startSpan(url)
-            span.setTag("http.method", method);
-            span.setTag("http.referer", referer);
-            span.setTag("http.user-agent", ua);
-            span.setTag("http.ip", ip);
-            console.log(req.body)
-            // req.body['rootSpan'] = req.egContext.run(span)
-            // let bodyData = JSON.stringify(req.body)
-            // req.egContext.requestStream = new PassThrough();
-            // req.egContext.requestStream.write(bodyData);
+            let contentType = 'application/x-www-form-urlencoded';
+            jsonParser(req, res, (err) => {
+                if (err) return next(err);
+                if (req.body !== {}) contentType = 'application/json';
+                urlEncoded(req, res, (err) => {
+                    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+                    var method = req.method;
+                    var url = req.url;
+                    var referer = req.headers.referer || "";
+                    var ua = req.headers['user-agent'];
+                    let span = tracer.startSpan(url)
+                    span.setTag("http.method", method);
+                    span.setTag("http.referer", referer);
+                    span.setTag("http.user-agent", ua);
+                    span.setTag("http.ip", ip);
+                    req.body['rootSpan'] = req.egContext.run(span)
+                    let bodyData = JSON.stringify(req.body)
+                    req.egContext.requestStream = new PassThrough();
+                    req.egContext.requestStream.write(bodyData);
+                    req.headers['content-length'] = Buffer.byteLength(bodyData);
+                    req.headers['content-type'] = contentType;
 
-            res.on('finish', () => {
-                var code = res._header ? String(res.statusCode) : String(-1);
-                var message = res._header ? String(res.statusMessage) : String(-1);
-                span.setTag("http.status_code", code);
-                if(req.headers && req.headers['ad-userid'])
-                    span.setTag("http.user-id", req.headers['ad-userid']);
-                span.setTag("http.status_message", message);
-                span.log({ 'event': 'request_end' });
-                span.finish();
-            });
-            next()
+                    res.on('finish', () => {
+                        var code = res._header ? String(res.statusCode) : String(-1);
+                        var message = res._header ? String(res.statusMessage) : String(-1);
+                        span.setTag("http.status_code", code);
+                        if (req.headers && req.headers['ad-userid'])
+                            span.setTag("http.user-id", req.headers['ad-userid']);
+                        span.setTag("http.status_message", message);
+                        span.log({ 'event': 'request_end' });
+                        span.finish();
+                    });
+                    next()
+                })
+            })
         };
     }
 };
